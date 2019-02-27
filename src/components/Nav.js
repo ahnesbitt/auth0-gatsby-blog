84481ba5465f57e37ab2670757f3917d99a51e67
Sam Julien <sam.julien@auth0.com>
2019-02-27 14:46:33 -0800

    Update auth and subscription code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,110 +1,103 @@
-import React from 'react';
-import Auth from '../utils/auth';
-import Subscribe from './Subscribe';
+import React from 'react'
+import auth from '../utils/auth'
+import Subscribe from './Subscribe'
 
-import logo from '../assets/logo-100-blue.png';
-
-const auth = new Auth();
+import logo from '../assets/logo-100-blue.png'
 
 export default class Nav extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      authenticated: false
-    };
+      authenticated: false,
+    }
   }
 
   login() {
-    auth.login();
+    auth.login()
 
     this.setState({
-      authenticated: auth.isAuthenticated()
-    });
+      authenticated: auth.isAuthenticated(),
+    })
   }
 
   logout() {
-    auth.logout();
+    auth.logout()
 
     this.setState({
-      authenticated: auth.isAuthenticated()
-    });
+      authenticated: auth.isAuthenticated(),
+    })
   }
 
   componentDidMount() {
     this.setState({
-      authenticated: auth.isAuthenticated()
-    });
+      authenticated: auth.isAuthenticated(),
+    })
   }
 
   render() {
     return (
       <div
         style={{
-          height: '50px'
+          height: '50px',
         }}
       >
         <div
           style={{
             float: 'left',
             lineHeight: '37px',
-            height: '37px'
+            height: '37px',
           }}
         >
           <img src={logo} alt="Auth0" />
         </div>
         <div
           style={{
-            float: 'right'
+            float: 'right',
           }}
         >
-          <a href="/"
-             style={{
-               boxShadow: 'none',
-               lineHeight: '37px'
-             }}
+          <a
+            href="/"
+            style={{
+              boxShadow: 'none',
+              lineHeight: '37px',
+            }}
           >
             Home
           </a>
           <span> | </span>
-          {
-            !this.state.authenticated && (
-              <span>
-                <a href="#"
-                  onClick={this.login.bind(this)}
-                  style={{
-                    boxShadow: 'none',
-                    lineHeight: '37px'
-                  }}
-                >
-                  Log In
-                </a>
-              </span>
-            )
-          }
-          {
-            this.state.authenticated && (
-              <span>
-                <a href="#"
-                  onClick={this.logout.bind(this)}
-                  style={{
-                    boxShadow: 'none',
-                    lineHeight: '37px'
-                  }}
-                >
-                  Log Out
-                  {
-                    auth.getUserName() && (
-                      <span> ({auth.getUserName()})</span>
-                    )
-                  }
-                </a>
-                <span> | </span>
-                <Subscribe />
-              </span>
-            )
-          }
+          {!this.state.authenticated && (
+            <span>
+              <a
+                href="#"
+                onClick={this.login.bind(this)}
+                style={{
+                  boxShadow: 'none',
+                  lineHeight: '37px',
+                }}
+              >
+                Log In
+              </a>
+            </span>
+          )}
+          {this.state.authenticated && (
+            <span>
+              <a
+                href="#"
+                onClick={this.logout.bind(this)}
+                style={{
+                  boxShadow: 'none',
+                  lineHeight: '37px',
+                }}
+              >
+                Log Out
+                {auth.getUserName() && <span> ({auth.getUserName()})</span>}
+              </a>
+              <span> | </span>
+              <Subscribe auth={auth} />
+            </span>
+          )}
         </div>
       </div>
-    );
+    )
   }
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,77 +1,107 @@
-import auth0 from 'auth0-js';
-import { navigateTo } from "gatsby-link";
+import auth0 from 'auth0-js'
+import { navigateTo } from 'gatsby-link'
 
-const AUTH0_DOMAIN = '<your-domain>.auth0.com';
-const AUTH0_CLIENT_ID = '<your-client-id>';
+const AUTH0_DOMAIN = '<your-domain>.auth0.com'
+const AUTH0_CLIENT_ID = '<your-client-id>'
 
-export default class Auth {
+class Auth {
+  accessToken
+  idToken
+  expiresAt
+  userProfile
+
   auth0 = new auth0.WebAuth({
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
     redirectUri: 'http://localhost:8000/callback',
     audience: `https://${AUTH0_DOMAIN}/api/v2/`,
     responseType: 'token id_token',
-    scope: 'openid profile email'
-  });
+    scope: 'openid profile email',
+  })
 
   constructor() {
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
+    this.handleAuthentication = this.handleAuthentication.bind(this)
+    this.isAuthenticated = this.isAuthenticated.bind(this)
+    this.getAccessToken = this.getAccessToken.bind(this)
+    this.getIdToken = this.getIdToken.bind(this)
   }
 
   login() {
-    this.auth0.authorize();
-  }
-
-  logout() {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
-    localStorage.removeItem('user');
+    this.auth0.authorize()
   }
 
   handleAuthentication() {
-    if (typeof window !== 'undefined') {
-      this.auth0.parseHash((err, authResult) => {
-        if (authResult && authResult.accessToken && authResult.idToken) {
-          this.setSession(authResult);
-        } else if (err) {
-          console.log(err);
-        }
-
-        // Return to the homepage after authentication.
-        navigateTo('/');
-      });
-    }
-  }
-
-  isAuthenticated() {
-    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-
-  setSession(authResult) {
-    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
-
-    this.auth0.client.userInfo(authResult.accessToken, (err, user) => {
-      localStorage.setItem('user', JSON.stringify(user));
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult)
+      } else if (err) {
+        navigateTo('/')
+        console.log(err)
+        alert(`Error: ${err.error}. Check the console for further details.`)
+      }
     })
   }
 
+  getAccessToken() {
+    return this.accessToken
+  }
+
+  getIdToken() {
+    return this.idToken
+  }
+
+  setSession(authResult) {
+    // Set isLoggedIn flag in localStorage
+    localStorage.setItem('isLoggedIn', 'true')
+
+    // Set the time that the access token will expire at
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime()
+    this.accessToken = authResult.accessToken
+    this.idToken = authResult.idToken
+    this.expiresAt = expiresAt
+
+    this.auth0.client.userInfo(this.accessToken, (err, profile) => {
+      if (profile) {
+        this.userProfile = profile
+      }
+      // navigateTo to the home route
+      navigateTo('/')
+    })
+  }
+
+  logout() {
+    // Remove tokens and expiry time
+    this.accessToken = null
+    this.idToken = null
+    this.expiresAt = 0
+    this.userProfile = null
+
+    // Remove isLoggedIn flag from localStorage
+    localStorage.removeItem('isLoggedIn')
+
+    // navigateTo to the home route
+    navigateTo('/')
+  }
+
+  isAuthenticated() {
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = this.expiresAt
+    return new Date().getTime() < expiresAt
+  }
+
   getUser() {
-    if (localStorage.getItem('user')) {
-      return JSON.parse(localStorage.getItem('user'));
-    }
+    return this.userProfile
   }
 
   getUserName() {
     if (this.getUser()) {
-      return this.getUser().name;
+      return this.getUser().name
     }
   }
 }
+
+const auth = new Auth()
+export default auth
